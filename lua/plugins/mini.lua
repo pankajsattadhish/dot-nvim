780e19aa12be4a975b08d0a17154bac84e6b8b46
Pankaj Patil <pankajsattadhish@gmail.com>
2026-02-14 22:49:41 +0530

    refactor: restructure mini.nvim config with mini.pick comments

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,22 +1,106 @@
 return {
 
-	-- Mini.nvim core modules
+	-- Mini.nvim (Core + Pick)
 	{
 		"echasnovski/mini.nvim",
-
 		version = false,
+
+		-- Lazy-load picker on key usage
+		-- keys = {
+		--   -- Files
+		--   {
+		--     "<leader>pf",
+		--     function() require("mini.pick").builtin.files() end,
+		--     desc = "Files",
+		--   },
+		--   {
+		--     "<C-p>",
+		--     function() require("mini.pick").builtin.files() end,
+		--     desc = "Files",
+		--   },
+		--
+		--   -- Live Grep
+		--   {
+		--     "<leader>ps",
+		--     function() require("mini.pick").builtin.grep_live() end,
+		--     desc = "Live Grep",
+		--     mode = { "n", "x" },
+		--   },
+		--
+		--   -- Buffers
+		--   {
+		--     "<leader>bo",
+		--     function() require("mini.pick").builtin.buffers() end,
+		--     desc = "Buffers",
+		--   },
+		--
+		--   -- Diagnostics
+		--   {
+		--     "<leader>dd",
+		--     function() require("mini.pick").builtin.diagnostics() end,
+		--     desc = "Diagnostics",
+		--   },
+		--   {
+		--     "<leader>dD",
+		--     function()
+		--       require("mini.pick").builtin.diagnostics({ scope = "current" })
+		--     end,
+		--     desc = "Buffer Diagnostics",
+		--   },
+		--
+		--   -- Quickfix / Location list
+		--   {
+		--     "<leader>dq",
+		--     function() require("mini.pick").builtin.quickfix() end,
+		--     desc = "Quickfix",
+		--   },
+		--   {
+		--     "<leader>dl",
+		--     function() require("mini.pick").builtin.loclist() end,
+		--     desc = "Location List",
+		--   },
+		--
+		--   -- LSP
+		--   {
+		--     "<leader>pr",
+		--     function() require("mini.pick").builtin.lsp_references() end,
+		--     desc = "References",
+		--   },
+		--   {
+		--     "<leader>ls",
+		--     function() require("mini.pick").builtin.lsp_symbols() end,
+		--     desc = "Document Symbols",
+		--   },
+		--   {
+		--     "<leader>lS",
+		--     function() require("mini.pick").builtin.lsp_workspace_symbols() end,
+		--     desc = "Workspace Symbols",
+		--   },
+		--
+		--   -- Help
+		--   {
+		--     "<leader>ph",
+		--     function() require("mini.pick").builtin.help() end,
+		--     desc = "Help",
+		--   },
+		-- },
+
 		config = function()
 			require("mini.surround").setup()
 			require("mini.pairs").setup()
 			require("mini.bracketed").setup()
 			require("mini.trailspace").setup()
+			-- require("mini.pick").setup()
 		end,
 	},
 
-	-- Icons (lazy)
-	{ "echasnovski/mini.icons", lazy = true },
+	-- Mini Icons (optional)
+	{
+		"echasnovski/mini.icons",
+		lazy = true,
+	},
 
-	-- MiniDiff (REPLACE gitsigns)
+	-- Mini Diff (Git signs)
 	{
 		"echasnovski/mini.diff",
 		version = false,
@@ -34,9 +118,7 @@ return {
 				},
 			})
 
-			-- Auto-refresh is handled automatically by mini.diff
-
-			-- KEYMAPS
+			-- Hunk navigation
 			vim.keymap.set("n", "]h", function()
 				diff.goto_hunk("next")
 			end, { desc = "Next Hunk" })
