c773632b37bd3509e6caa8557657b6fe5453bafb
Pankaj Patil <pankajsattadhish@gmail.com>
2026-02-21 13:41:03 +0530

    refactor: disable indent-blankline.nvim in UI plugins

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -16,14 +16,14 @@ return {
 		end,
 	},
 
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-		---@module "ibl"
-		---@type ibl.config
-		opts = {},
-		config = function()
-			require("ibl").setup()
-		end,
-	},
+	-- {
+	-- 	"lukas-reineke/indent-blankline.nvim",
+	-- 	main = "ibl",
+	-- 	---@module "ibl"
+	-- 	---@type ibl.config
+	-- 	opts = {},
+	-- 	config = function()
+	-- 		require("ibl").setup()
+	-- 	end,
+	-- },
 }
